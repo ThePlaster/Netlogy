b62108ae4a6d63601ea83b46797f874bd8b67956
ThePlaster <47009557+ThePlaster@users.noreply.github.com>
2019-02-05 13:01:23 +0300

    Update Польская нотация.py

--- Польская нотация.py
+++ Польская нотация.py
@@ -1,37 +1,40 @@
+def calc():
+    try:
+        print('Это программа вычеслений в формате польской нотации!')
+        print('Введите знак(+,-,*,/), а затем два числа')
+        command, num_1, num_2 = input('Введите выражение: ').split(' ')
 
-try:
-    print('Это программа вычеслений в формате польской нотации!')
-    print('Введите знак(+,-,*,/), а затем два числа')
-    command, num_1, num_2 = input('Введите выражение: ').split(' ')
-
-except ValueError:
-  print('Вы ввели не правильное число аргументов!')
+    except ValueError:
+      print('Вы ввели не правильное число аргументов!')
 
 
-try:
-    assert command == '+' or command == '-' \
-        or command == '*' or command == '/', \
-        'Программа поддерживает только опперации "+", "-", "*", "/"'
-    if command == '+':
-        num_result = int(num_1) + int(num_2)
-    if command == '-':
-        num_result = int(num_1) - int(num_2)
-    if command == '*':
-        num_result = int(num_1) * int(num_2)
-    if command == '/':
-        num_result = int(num_1) / int(num_2)
+    try:
+        assert command == '+' or command == '-' \
+            or command == '*' or command == '/', \
+            'Программа поддерживает только опперации "+", "-", "*", "/"'
+        if command == '+':
+            num_result = int(num_1) + int(num_2)
+        if command == '-':
+            num_result = int(num_1) - int(num_2)
+        if command == '*':
+            num_result = int(num_1) * int(num_2)
+        if command == '/':
+            num_result = int(num_1) / int(num_2)
 
-except NameError:
-    print('Ошибка, операция не найдена.')
+    except NameError:
+        print('Ошибка, операция не найдена.')
 
-except ZeroDivisionError:
-    print('На ноль делить нельзя!')
+    except ZeroDivisionError:
+        print('На ноль делить нельзя!')
 
-except ValueError:
-    print('Вы ввели некоректные данные!')
+    except ValueError:
+        print('Вы ввели некоректные данные!')
 
-try:
-    print('Результат:', num_1, command, num_2, '=', num_result)
+    try:
+        print('Результат:', num_1, command, num_2, '=', num_result)
 
-except NameError:
-    print('Ненайден ответ, вы ввели некорректные данные')
+    except NameError:
+        print('Ненайден ответ, вы ввели некорректные данные')
+ 
+
+calc()
