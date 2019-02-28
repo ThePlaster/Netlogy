f7ba65dcce34fdda13ddec83639f08dd5486028d
ThePlaster <47009557+ThePlaster@users.noreply.github.com>
2019-02-28 14:22:42 +0300

    Add files via upload

---
+++ Lesson_9/Menu_1_5.py
@@ -0,0 +1,46 @@
+def read_menu():
+
+  cook_book = {}
+
+  with open ('Eat_list.txt') as eat_list:
+    for line in eat_list:
+      name = line.strip()
+      num = int(eat_list.readline())
+      cook_book.setdefault(name,[])
+
+      for loop in range(num):
+        ing = eat_list.readline().strip().split(' | ')
+        cook_book[name].append({'ingridient_name':ing[0],
+        'quantity':int(ing[1]), 'measure':ing[2]})
+
+      eat_list.readline()
+
+  return cook_book
+
+dishes = []
+person_count =()
+
+def get_shop_list_by_dishes(dishes, person_count):
+
+  cook = read_menu()
+  dich_dict = {}
+
+  for food_name in dishes:
+    for ingrid in cook.get(food_name):
+      all_data = {'quantity':ingrid.get('quantity')*person_count,
+      'measure':ingrid.get('measure')}
+
+      if ingrid.get('ingridient_name') in dich_dict:
+        name_ingr = ingrid.get('ingridient_name')
+        quantity = dich_dict.get(name_ingr).get('quantity') \
+          + ingrid.get('quantity')*person_count
+        dich_dict.update({name_ingr:{'quantity':quantity,
+          'measure':ingrid.get('measure')}})
+
+      dich_dict.setdefault(ingrid.get('ingridient_name'), all_data)
+
+  print(dich_dict, sep = '\n')
+
+
+read_menu()
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3)
